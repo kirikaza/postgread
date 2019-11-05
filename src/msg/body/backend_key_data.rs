16ddeaeda0e6717180f7954a9030ae608cc6330d
Kirill Kazakov <k@kirikaza.ru>
2019-11-06 00:43:56 +0400

    Implement reading of backend msg BackendKeyData

---
+++ src/msg/body/backend_key_data.rs
@@ -0,0 +1,46 @@
+use super::super::io::*;
+use ::futures::io::AsyncBufReadExt;
+use ::std::io::Result as IoResult;
+
+#[derive(Debug, PartialEq)]
+pub struct BackendKeyData {
+    process_id: u32,
+    secret_key: u32,
+}
+
+impl BackendKeyData {
+    pub const TYPE_BYTE: Option<u8> = Some(b'K');
+
+    pub async fn read<R>(stream: &mut R, _body_len: u32) -> IoResult<Self>
+    where R: AsyncBufReadExt + Unpin
+    {
+        let process_id = read_u32(stream).await?;
+        let secret_key = read_u32(stream).await?;
+        Ok(Self { process_id, secret_key })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{BackendKeyData};
+    use crate::msg::BackendMessage;
+    use crate::msg::test_util::*;
+
+    #[test]
+    fn simple() {
+        let mut bytes: &[u8] = &[
+            b'K',
+            0, 0, 0, 12,  // len
+            0x1, 0x2, 0x3, 0x4,  // process ID
+            0x5, 0x6, 0x7, 0x8,  // secret key
+        ];
+        assert_eq!(
+            ok_some(BackendKeyData { process_id: 0x01020304, secret_key: 0x05060708 }),
+            force_read_backend(&mut bytes),
+        );
+    }
+
+    fn ok_some(body: BackendKeyData) -> Result<Option<BackendMessage>, String> {
+        ok_some_msg(body, BackendMessage::BackendKeyData)
+    }
+}
