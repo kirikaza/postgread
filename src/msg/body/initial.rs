4f3a14977d5f0b7121174c149412ec4143d89b9b
Kirill Kazakov <k@kirikaza.ru>
2019-11-17 22:40:35 +0400

    Implement reading of frontend messages CancelRequest and SSLRequest

--- src/msg/body/initial.rs
+++ src/msg/body/initial.rs
@@ -4,20 +4,43 @@ use ::std::fmt::{self, Debug, Formatter};
 use ::std::io::Result as IoResult;
 
 #[derive(Debug, PartialEq)]
+pub enum Initial {
+    Cancel(Cancel),
+    SSL,
+    Startup(Startup),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Cancel {
+    process_id: u32,
+    secret_key: u32,
+}
+
+#[derive(Debug, PartialEq)]
 pub struct Startup {
     version: Version,
     params: Vec<StartupParam>,
 }
 
-impl Startup {
+impl Initial {
     pub const TYPE_BYTE: Option<u8> = None;
 
     pub async fn read<R>(stream: &mut R, _body_len: u32) -> IoResult<Self>
     where R: AsyncBufReadExt + Unpin
     {
-        let version = Version::read(stream).await?;
-        let params = StartupParam::read_many(stream).await?;
-        Ok(Startup { version, params })
+        match Version::read(stream).await? {
+            Version { major: 1234, minor: 5678 } => {
+                let process_id = read_u32(stream).await?;
+                let secret_key = read_u32(stream).await?;
+                Ok(Self::Cancel(Cancel { process_id, secret_key }))
+            },
+            Version { major: 1234, minor: 5679 } =>
+                Ok(Self::SSL),
+            version => {
+                let params = StartupParam::read_many(stream).await?;
+                Ok(Self::Startup(Startup { version, params }))
+            }
+        }
     }
 }
 
@@ -75,28 +98,54 @@ impl Debug for StartupParam {
 
 #[cfg(test)]
 mod tests {
-    use super::{Startup, StartupParam, Version};
+    use super::{Cancel, Initial, Startup, StartupParam, Version};
     use crate::msg::FrontendMessage;
     use crate::msg::test_util::*;
 
     #[test]
-    fn without_params() {
+    fn cancel() {
+        let mut bytes: &[u8] = &[
+            0, 0, 0, 16, // len
+            4, 210, 22, 46, // 4*256+210=1234, 22*256+46=5678, these numbers instead of version mean "cancel"
+            0x1, 0x2, 0x3, 0x4,  // process ID
+            0x5, 0x6, 0x7, 0x8,  // secret key
+        ];
+        assert_eq!(
+            ok_some(Initial::Cancel(Cancel { process_id: 0x01020304, secret_key: 0x05060708 })),
+            force_read_frontend(&mut bytes, true),
+        );
+    }
+
+    #[test]
+    fn ssl() {
+        let mut bytes: &[u8] = &[
+            0, 0, 0, 8, // len
+            4, 210, 22, 47, // 4*256+210=1234, 22*256+47=5679, these numbers instead of version mean "SSL"
+        ];
+        assert_eq!(
+            ok_some(Initial::SSL),
+            force_read_frontend(&mut bytes, true),
+        );
+    }
+
+    #[test]
+    fn startup_without_params() {
         let mut bytes: &[u8] = &[
             0, 0, 0, 9, // len
             0, 3, 0, 1, // version
             0, // params
         ];
         assert_eq!(
-            ok_some(Startup {
+            ok_some(Initial::Startup(Startup {
                 version: Version { major: 3, minor: 1 },
                 params: vec![],
-            }),
+            })),
             force_read_frontend(&mut bytes, true),
         );
     }
 
     #[test]
-    fn with_params() {
+    fn startup_with_params() {
         let mut bytes = vec![
             0, 0, 0, 37, // len
             0, 3, 1, 0, // version
@@ -104,7 +153,7 @@ mod tests {
         bytes.extend_from_slice(b"user\0root\0database\0postgres\0\0");
         let mut bytes = &bytes[..];
         assert_eq!(
-            ok_some(Startup {
+            ok_some(Initial::Startup(Startup {
                 version: Version { major: 3, minor: 0x100 },
                 params: vec![
                     StartupParam {
@@ -116,12 +165,12 @@ mod tests {
                         value: Vec::from(&b"postgres"[..]),
                     },
                 ],
-            }),
+            })),
             force_read_frontend(&mut bytes, true),
         );
     }
 
-    fn ok_some(body: Startup) -> Result<Option<FrontendMessage>, String> {
-        ok_some_msg(body, FrontendMessage::Startup)
+    fn ok_some(body: Initial) -> Result<Option<FrontendMessage>, String> {
+        ok_some_msg(body, FrontendMessage::Initial)
     }
 }
