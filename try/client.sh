d5673e947440277766357a9c2471d3251bc269d1
Kirill Kazakov <k@kirikaza.ru>
2020-02-16 21:57:55 +0400

    Add self-signed certificate and run.sh to use this certificate, modify client.sh to control sslmode option

--- try/client.sh
+++ try/client.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 
-docker run -it --rm --network host postgres:alpine psql 'sslmode=disable host=localhost port=15432 user=postgres' "$@"
+ssl_mode="$1"
+shift
+docker run -it --rm --network host postgres:alpine psql "$ssl_mode host=localhost port=15432 user=postgres" "$@"
