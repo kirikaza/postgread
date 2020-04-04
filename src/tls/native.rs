58e56f0cc04414070046cdcbca11f4b126445744
Kirill Kazakov <k@kirikaza.ru>
2020-04-04 22:47:10 +0400

    Implement TLS with backend, modify try/server.sh to enable and log TLS

--- src/tls/native.rs
+++ src/tls/native.rs
@@ -1,18 +1,41 @@
 use crate::tls::interface::*;
 
-use ::async_native_tls::{TlsAcceptor, TlsStream};
+use ::async_native_tls::{TlsAcceptor, TlsConnector, TlsStream};
 use ::async_trait::async_trait;
 use ::futures::io::{AsyncRead, AsyncWrite};
 
-pub struct NativeTlsProvider<'a>(pub &'a TlsAcceptor);
+pub struct NativeTlsClient<'a> {
+    pub connector: &'a TlsConnector,
+    pub hostname: &'a str,
+}
 
-#[async_trait]
-impl<'a, Plain> TlsProvider<Plain> for NativeTlsProvider<'a>
+pub struct NativeTlsServer<'a>(pub &'a TlsAcceptor);
+
+impl<'a, Plain> TlsProvider<Plain> for NativeTlsClient<'a>
 where Plain: AsyncRead + AsyncWrite + Send + Unpin {
     type Tls = TlsStream<Plain>;
     type Error = ::native_tls::Error;
+}
 
-    async fn handshake(self: &Self, plain: Plain) -> Result<TlsStream<Plain>, ::native_tls::Error>
+impl<'a, Plain> TlsProvider<Plain> for NativeTlsServer<'a>
+where Plain: AsyncRead + AsyncWrite + Send + Unpin {
+    type Tls = TlsStream<Plain>;
+    type Error = ::native_tls::Error;
+}
+
+#[async_trait]
+impl<'a, Plain> TlsClient<Plain> for NativeTlsClient<'a>
+where Plain: AsyncRead + AsyncWrite + Send + Unpin {
+    async fn connect(self: &Self, plain: Plain) -> Result<TlsStream<Plain>, ::native_tls::Error>
+    where Plain: 'async_trait {
+        self.connector.connect(self.hostname, plain).await
+    }
+}
+
+#[async_trait]
+impl<'a, Plain> TlsServer<Plain> for NativeTlsServer<'a>
+where Plain: AsyncRead + AsyncWrite + Send + Unpin {
+    async fn accept(self: &Self, plain: Plain) -> Result<TlsStream<Plain>, ::native_tls::Error>
     where Plain: 'async_trait {
         self.0.accept(plain).await
     }
--- try/server.sh
+++ try/server.sh
@@ -1,3 +1,13 @@
 #! /bin/sh
 
-docker run -it --rm --network host postgres:alpine
+cd "`dirname "$0"`" || exit 1
+vm_dir=/var/lib/postgresql
+docker run -it --rm --network host \
+    -v "$PWD"/cert.pem:$vm_dir/cert.pem \
+    -v "$PWD"/key.pem:$vm_dir/key.pem \
+    postgres:alpine \
+    -c ssl=on \
+    -c ssl_cert_file=$vm_dir/cert.pem \
+    -c ssl_key_file=$vm_dir/key.pem \
+    -c log_min_messages=DEBUG2 |  # to log SSL connections
+    grep -v 'writing stats file'  # too many such lines
