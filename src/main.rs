4f4757be5848024a056f20c3c8e63a385badb70d
Kirill Kazakov <me@kirikaza.dev>
2021-03-28 23:57:54 +0400

    Refactor: parameterize fn loop_accepting with a callback

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,26 @@ extern crate postgread;
 extern crate structopt;
 
 use postgread::server::{self, Config};
+use postgread::convey::Message;
 
 use async_std::task;
 use std::io;
+use std::sync::Arc;
 use structopt::StructOpt;
+
+fn dump_msg(msg: Message) {
+    match msg {
+        Message::Backend(backend_msg) =>
+            println!("postgread got from server {:?}", backend_msg),
+        Message::Frontend(frontend_msg) =>
+            println!("postgread got from client {:?}", frontend_msg),
+    }
+}
 
 fn main() -> io::Result<()> {
     let config = Config::from_args();
     task::block_on(async {
         let server = server::listen(config).await?;
-        server::loop_accepting(server).await
+        server::loop_accepting(server, Arc::new(dump_msg)).await
     })
 }
